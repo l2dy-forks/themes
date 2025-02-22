2a1ca06c66cbd2b2b124b8df415a24ef1f31c2be
Zero King <l2dy@icloud.com>
2025-02-23 02:17:57 +0800

    Nord: lowercase

--- themes/Nord.js
+++ themes/Nord.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides', ['#3B4252', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#88C0D0', '#E5E9F0', '#4C566A', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#8FBCBB', '#ECEFF4']);
-t.prefs_.set('foreground-color', '#D8DEE9');
-t.prefs_.set('background-color', '#2E3440');
+t.prefs_.set('color-palette-overrides', ['#3b4252', '#bf616a', '#a3be8c', '#ebcb8b', '#81a1c1', '#b48ead', '#88c0d0', '#e5e9f0', '#4c566a', '#bf616a', '#a3be8c', '#ebcb8b', '#81a1c1', '#b48ead', '#8fbcbb', '#eceff4']);
+t.prefs_.set('foreground-color', '#d8dee9');
+t.prefs_.set('background-color', '#2e3440');
 t.prefs_.set('cursor-color', 'rgb(216 222 233 / 50%)');
