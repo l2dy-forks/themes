a2ba18cf9699b46edb6e230f9f5d22e927d16ba3
Zero King <l2dy@icloud.com>
2025-02-23 02:09:40 +0800

    Nord: remove const definitions

--- themes/Nord.js
+++ themes/Nord.js
@@ -4,68 +4,17 @@
  * Originally released under the MIT license.
  */
 
-const nord = {
-  nord0: "#2E3440",
-  nord1: "#3B4252",
-  nord2: "#434C5E",
-  nord3: "#4C566A",
-  nord4: "#D8DEE9",
-  nord5: "#E5E9F0",
-  nord6: "#ECEFF4",
-  nord7: "#8FBCBB",
-  nord8: "#88C0D0",
-  nord9: "#81A1C1",
-  nord10: "#5E81AC",
-  nord11: "#BF616A",
-  nord12: "#D08770",
-  nord13: "#EBCB8B",
-  nord14: "#A3BE8C",
-  nord15: "#B48EAD"
-};
-
-
-const colors = {
-  black: nord.nord1,
-  red: nord.nord11,
-  green: nord.nord14,
-  yellow: nord.nord13,
-  blue: nord.nord9,
-  magenta: nord.nord15,
-  cyan: nord.nord8,
-  white: nord.nord5,
-  lightBlack: nord.nord3,
-  lightRed: nord.nord11,
-  lightGreen: nord.nord14,
-  lightYellow: nord.nord13,
-  lightBlue: nord.nord9,
-  lightMagenta: nord.nord15,
-  lightCyan: nord.nord7,
-  lightWhite: nord.nord6,
-
-  backgroundColor: nord.nord0,
-  foregroundColor: nord.nord4,
-  cursorColor: "rgb(216 222 233 / 50%)", // nord4
-};
-
 t.prefs_.set('color-palette-overrides', [
-    colors.black,
-    colors.red,
-    colors.green,
-    colors.yellow,
-    colors.blue,
-    colors.magenta,
-    colors.cyan,
-    colors.white,
-    colors.lightBlack,
-    colors.lightRed,
-    colors.lightGreen,
-    colors.lightYellow,
-    colors.lightBlue,
-    colors.lightMagenta,
-    colors.lightCyan,
-    colors.lightWhite
+  '#3B4252', '#BF616A',
+  '#A3BE8C', '#EBCB8B',
+  '#81A1C1', '#B48EAD',
+  '#88C0D0', '#E5E9F0',
+  '#4C566A', '#BF616A',
+  '#A3BE8C', '#EBCB8B',
+  '#81A1C1', '#B48EAD',
+  '#8FBCBB', '#ECEFF4'
 ]);
 
-t.prefs_.set('cursor-color', colors.cursorColor);
-t.prefs_.set('foreground-color', colors.foregroundColor);
-t.prefs_.set('background-color', colors.backgroundColor);
+t.prefs_.set('cursor-color', 'rgb(216 222 233 / 50%)');
+t.prefs_.set('foreground-color', '#D8DEE9');
+t.prefs_.set('background-color', '#2E3440');
