cd38d6b11f2546bb13e8233f486583785454f246
Zero King <l2dy@icloud.com>
2025-02-23 02:15:13 +0800

    Nord: formatting

--- themes/Nord.js
+++ themes/Nord.js
@@ -1,14 +1,4 @@
-t.prefs_.set('color-palette-overrides', [
-  '#3B4252', '#BF616A',
-  '#A3BE8C', '#EBCB8B',
-  '#81A1C1', '#B48EAD',
-  '#88C0D0', '#E5E9F0',
-  '#4C566A', '#BF616A',
-  '#A3BE8C', '#EBCB8B',
-  '#81A1C1', '#B48EAD',
-  '#8FBCBB', '#ECEFF4'
-]);
-
-t.prefs_.set('cursor-color', 'rgb(216 222 233 / 50%)');
+t.prefs_.set('color-palette-overrides', ['#3B4252', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#88C0D0', '#E5E9F0', '#4C566A', '#BF616A', '#A3BE8C', '#EBCB8B', '#81A1C1', '#B48EAD', '#8FBCBB', '#ECEFF4']);
 t.prefs_.set('foreground-color', '#D8DEE9');
 t.prefs_.set('background-color', '#2E3440');
+t.prefs_.set('cursor-color', 'rgb(216 222 233 / 50%)');
